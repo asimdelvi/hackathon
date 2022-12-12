5e8a20a7033184211c859a4d9e0bfc25c1f548bc
asimdelvi <asimdelvi30@gmail.com>
2022-12-12 17:08:15 +0530

    demo change

--- controllers/city.js
+++ controllers/city.js
@@ -5,6 +5,7 @@ import { Hotel } from "../models/hotel.js";
 // Read all
 export const showAllCities = async (req, res, next) => {
   const cities = await City.find({});
+  console.log("hi");
   res.render("cities/index", { cities });
 };
 
